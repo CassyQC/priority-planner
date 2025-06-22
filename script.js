1a3235fd6fede216d055efeef25b59e893f3f9e8
Cassycq <cassycq@gmail.com>
2025-06-22 22:49:44 +1000

    Priority Planner

---
+++ script.js
@@ -0,0 +1,31 @@
+const tasks = ['']
+console.log('tasks',tasks)
+
+function addTask(){
+    console.log('add task')
+    const taskInput = document.getElementById('taskInput');
+    const priorityInput = document.getElementById('priorityInput');
+    const priority = parseInt(priorityInput.value); 
+    const task = taskInput.value.trim();
+    console.log('task',task)
+    if(!task){
+        alert('Please enter a task');
+        return;
+    }
+    let importance = '';
+    if(priority <= 2){
+        importance = 'high'
+    }else if (priority === 3){
+        importance = 'medium'
+    }else{
+        importance = 'low'
+    }
+    const newTask = {
+        'task' : task,
+        'priority' : priority,
+        'importance':importance
+    }
+    tasks.push(newTask)
+    console.log('tasks',tasks)
+    taskInput.value = '';
+}
